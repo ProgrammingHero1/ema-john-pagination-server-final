c80deee79f06bb1c51949ac80a76f1bfdba39b0f
Programming Hero <programming.hero1@gmail.com>
2023-05-12 20:38:19 -0500

    created total products api to load product count

--- index.js
+++ index.js
@@ -35,6 +35,11 @@ async function run() {
         res.send(result);
     })
 
+    app.get('/totalProducts', async(req, res) =>{
+      const result = await productCollection.estimatedDocumentCount();
+      res.send({totalProducts: result})
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
