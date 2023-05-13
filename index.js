61d5c0a4d645d9031fc1018d9dd9fb1a276f0824
Programming Hero <programming.hero1@gmail.com>
2023-05-12 22:47:52 -0500

    api to load some products by ids using in operator

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -43,6 +43,15 @@ async function run() {
     app.get('/totalProducts', async (req, res) => {
       const result = await productCollection.estimatedDocumentCount();
       res.send({ totalProducts: result })
+    })
+
+    app.post('/productsByIds', async(req, res) => {
+      const ids = req.body;
+      const objectIds = ids.map(id => new ObjectId(id));
+      const query = { _id: { $in: objectIds } }
+      console.log(ids);
+      const result = await productCollection.find(query).toArray();
+      res.send(result);
     })
 
     // Send a ping to confirm a successful connection
